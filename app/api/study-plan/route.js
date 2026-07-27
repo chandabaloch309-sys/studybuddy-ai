dc717269e5ffd57f5f4335b946fa309c8c83c0cd
chandabaloch309-sys <chandabaloch309@gmail.com>
2026-07-27 13:51:32 +0500

    Update route.js

--- app/api/study-plan/route.js
+++ app/api/study-plan/route.js
@@ -12,7 +12,7 @@ export async function POST(req) {
 
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash",});
+    const model = genAI.getGenerativeModel({ model: "gemini-3.5-flash-lite",});
 
     const prompt = `Create a study plan for ${title}`;
 
